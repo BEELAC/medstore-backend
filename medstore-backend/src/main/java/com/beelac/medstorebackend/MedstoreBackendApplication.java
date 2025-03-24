651cb4f392ad7fd02ca7148b5e9f47469644274c
Caleb DeGeorge <cdegeorge96@gmail.com>
2025-03-24 13:02:10 -0400

    Initial commit with correct line endings

---
+++ medstore-backend/src/main/java/com/beelac/medstorebackend/MedstoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.beelac.medstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedstoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedstoreBackendApplication.class, args);
+	}
+
+}
---
+++ medstore-backend/src/test/java/com/beelac/medstorebackend/MedstoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beelac.medstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedstoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
